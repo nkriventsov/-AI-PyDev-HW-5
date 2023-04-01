7cafaba2d45fae50d39f2f77eb2d9ed60081cd2d
nkriventsov <61682500+nkriventsov@users.noreply.github.com>
2023-04-01 22:21:58 +0300

    ДЗ к 8-ой лекции.

--- test_filemanager.py
+++ test_filemanager.py
@@ -2,20 +2,18 @@ import io
 import sys
 # from HW_3_Quiz_Game.victory import quiz_game
 
-# Если не вызывать тестируемую функцию, а прописать её в коде (ниже закомментировано и реализовано в sandbox.py),
-# то тест работает, в текущем написании тест не работает.
 from console_file_manager import print_lines
 
 
-# def print_lines():
-#     return print('-' * 50)
-
+@print_lines
+def sum_test():
+    return
 
 def test_print_lines():
     capturedOutput = io.StringIO()                              # Create StringIO object
     sys.stdout = capturedOutput                                 # and redirect stdout.
-    print_lines()                                               # Call function.
-    assert capturedOutput.getvalue() == str('-' * 50 + '\n')
+    sum_test()                                                  # Call function.
+    assert capturedOutput.getvalue() == str(('-' * 50 + '\n')*2)
     sys.stdout = sys.__stdout__                                 # Reset redirect.
     # print('Captured', capturedOutput.getvalue())              # Now works as before.
 
